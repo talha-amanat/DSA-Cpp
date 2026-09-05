bd196757e29b05a94cefc93fc35e080cf1a77a5b
Muhammad Talha Amanat <talhaphantom16@gmail.com>
2026-09-05 13:07:12 +0500

    trees bst practice

--- trees.cpp
+++ trees.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
-struct treeNode{
+struct treenode{
     int data;
-    treeNode* left;
-    treeNode* right;
+    treenode* left;
+    treenode* right;
 
-    treeNode(int d){
+    treenode(int d){
         data=d;
         left=nullptr;
         right=nullptr;
     }
 };
-void insert(treeNode*& root,int val){
+void insert(treenode*& root,int val){
     if(root==nullptr){
-        root= new treeNode(val);
+        root=new treenode(val);
         return;
     }
     if(val<root->data){
@@ -21,22 +21,74 @@ void insert(treeNode*& root,int val){
     }else{
         insert(root->right,val);
     }
-}
-void print(treeNode*& root){
+ }
+
+ void print(treenode* root){
     if(root==nullptr){
         return;
     }
+
     print(root->left);
     cout<<root->data<<endl;
     print(root->right);
-}
-int main(){
-    treeNode* root=nullptr;
+ }
+ bool search(treenode* root,int val){
+    if(root==nullptr){
+        return false;
+    }
 
+    if(val==root->data){return true;}
+    if(val<root->data){
+       return search(root->left,val);
+    }else{
+        return search(root->right,val);
+    }
+ }
+ void deletenode(treenode*& root,int val){
+    if(root==nullptr){
+        return ;
+    }
+    if(val<root->data){
+        deletenode(root->left,val);
+    }else if(val>root->data){
+        deletenode(root->right,val)
+    }else{
+
+        if(root->left==nullptr && root->right==nullptr){
+            delete root;
+            root=nullptr;
+            return;
+        }
+        
+        if(root->left==nullptr){
+            treenode* temp=root;
+            root=root->right;
+            delete temp;
+            return;
+        }
+
+         if(root->right==nullptr){
+            treenode* temp=root;
+            root=root->left;
+            delete temp;
+            return;
+        }
+
+        treenode* successor=root->right;
+        while(successor->left!=nullptr){
+            successor=successor->left;
+        }
+        root->data=successor->data;
+        deletenode(root->right,successor->data);
+    }
+ }
+ int main(){
+    treenode* root=nullptr;
     insert(root,10);
-    insert(root,11);
-    insert(root,1);
-    insert(root,4);
+    insert(root,5);
+    insert(root,15);
+    insert(root,12);
+    insert(root,20);
 
     print(root);
-}
+ }
--- trees_practice.cpp
+++ trees_practice.cpp
@@ -17,20 +17,92 @@ void insert(treenode*& root,int val){
         root=new treenode(val);
         return;
     }
+    if(val<root->data){
+        return insert(root->left,val);
+    }else{
+        return insert(root->right,val);
+    }
+}
+
+bool search(treenode* root,int val){
+    if(root==nullptr){
+        return false;
+    }
+
+    if(val==root->data){
+        return true;
+    }
 
     if(val<root->data){
-        insert(root->left,val);
+       return search(root->left,val);
     }else{
-        insert(root->right,val);
+       return search(root->right,val);
+    }
+}
+
+void print(treenode* root){
+    if(root==nullptr){
+        return;
+    }
+    print(root->left);
+    cout<<root->data<<endl;
+    print(root->right);
+}
+
+void deletenode(treenode*& root,int val){
+    if(root==nullptr){
+        return;
+    }
+
+    if(val<root->data){
+        deletenode(root->left,val);
+    }else if(val>root->data){
+        deletenode(root->left,val);
+    }else{
+
+        if(root->left==nullptr && root->right==nullptr){
+            delete root;
+            root=nullptr;
+            return;
+        }
+
+        if(root->left==nullptr){
+            treenode* temp=root;
+            root=root->right;
+            delete temp;
+            return;
+        }
+
+        
+        if(root->right==nullptr){
+            treenode* temp=root;
+            root=root->left;
+            delete temp;
+            return;
+        }
+
+        treenode* successor=root->right;
+        while(successor->left != nullptr){
+            successor=successor->left;
+        }
+        root->data=successor->data;
+        deletenode(root->right,successor->data);
     }
 }
 int main(){
-    treenode* meow=nullptr;
+ treenode* root=nullptr;
+ 
+ insert(root,10);
+ insert(root,5);
+ insert(root,15);
+ insert(root,12);
+ insert(root,17);
 
-    insert(meow,5);
+ print(root);
 
-    treenode* temp=meow;
-    while(temp != nullptr){
-        cout<<temp->data
-    }
+cout<<search(root,15)<<endl;
+
+ deletenode(root,10);
+cout<<endl;
+ print(root);
 }
